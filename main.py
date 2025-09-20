fc005d10209df4315836fe4859f760f2e9cce330
jodieorogun <jodieorogun@gmail.com>
2025-09-20 20:53:28 +0100

    testing new .venv

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 import numpy as np
+"""
 from PIL import Image
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+"""
 
 def loadDataset():
     root = Path("data")
@@ -13,3 +15,6 @@ def loadDataset():
         print("hi")
         for pic in folder:
             print("yo")
+
+if __name__ == "__main__":
+    loadDataset()
