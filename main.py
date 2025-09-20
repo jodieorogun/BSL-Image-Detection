8e2b6171e1f1155914fd6d913b4a809567a078c4
jodieorogun <jodieorogun@gmail.com>
2025-09-20 19:51:41 +0100

    Create virtual environment (.venv)

---
+++ main.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import numpy as np
+from PIL import Image
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+def loadDataset():
+    root = Path("data")
+    X, Y = [], []
+    for folder in root.iterdir():
+        print("hi")
+        for pic in folder:
+            print("yo")
