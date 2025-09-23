0eb0dce081669aa50401e91a325763c999b2b713
jodieorogun <jodieorogun@gmail.com>
2025-09-23 13:52:02 +0100

    Added lots of new images and installed mediapipe

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from pathlib import Path
 import numpy as np
+import mediapipe as mp
+
+from pathlib import Path
 from PIL import Image
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
@@ -14,11 +16,12 @@ def loadDataset():
         for pic in folder.glob("*png"):
             image = Image.open(pic)
             image = image.resize((128, 128))
-            image = image.convert("RGB")
+            image = image.convert("L")
             arr = np.asarray(image, dtype=np.float32).flatten()
             X.append(arr)
             Y.append(folder.name)
     return X, Y
+
 
 def train(A, B):
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
@@ -30,7 +33,18 @@ def train(A, B):
     Y_pred = model.predict(X_test)
     accuracy = accuracy_score(Y_test_enc, Y_pred)
     print(f"Accuracy: {accuracy * 100:.2f}%")
+    return model, le
+
+def reshape(imagePath):
+    image = Image.open(imagePath)
+    image = image.resize((128, 128))
+    image = image.convert("L")
+    arr = np.asarray(image, dtype=np.float32).flatten()
+    return arr.reshape(1, -1)
 
 if __name__ == "__main__":
     X, Y = loadDataset()
-    train(X, Y)
+    model, le = train(X, Y)
+    pred = model.predict(reshape("my_prediction.png"))
+    predLabel = le.inverse_transform(pred)
+    print(f"Predicted label: {predLabel[0]}")
